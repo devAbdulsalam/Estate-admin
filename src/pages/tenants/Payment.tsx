cc2b28f81f0e5560799891b94341138887febc73
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-02-18 20:45:59 +0100

    add tenant pages

--- src/pages/tenants/Payment.tsx
+++ src/pages/tenants/Payment.tsx
@@ -6,12 +6,12 @@ import React from 'react';
 const Index = () => {
 	const { tokens } = useAuth();
 	const { data } = useQuery({
-		queryKey: ['home'],
+		queryKey: ['payments'],
 		queryFn: async () => fetchIndex(tokens?.token),
 	});
 	return (
 		<main className="min-h-screen bg-gray-50 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-			<div>Index</div>
+			<div>Payment</div>
 		</main>
 	);
 };
